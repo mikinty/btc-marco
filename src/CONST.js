8c4c7127ee4e9f211393c0c55908af61e0fb5b4b
mikinty <myou@andrew.cmu.edu>
2020-05-12 17:29:42 -0400

    Added color constants, first second derivatives, also simple trendline using start and end time price

--- src/CONST.js
+++ src/CONST.js
@@ -5,3 +5,9 @@ export const CHART_TIME_HRS = 6;
 export const GRANULARITY = [60, 300, 900, 3600, 21600, 86400][1];
 /** @brief Price refresh rate (in ms) */
 export const REFRESH_RATE = 200;
+
+/*** COLORS ***/
+export const BLUE_LIGHT = '#33ECFF';
+export const ORANGE_BITCOIN = '#F6921A';
+export const GREEN_SHREK = '#9CDE47';
+export const RED_CHINA = '#DE2910';
