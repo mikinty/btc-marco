85f1a8e4e78d97e0d5c004471208b0796f642305
mikinty <myou@andrew.cmu.edu>
2020-05-12 16:30:23 -0400

    Installed sass loader for style

--- src/CONST.js
+++ src/CONST.js
@@ -3,3 +3,5 @@ export const TICKER = 'BTC-USD';
 export const CHART_TIME_HRS = 6;
 /** @brief Granularity for the chart as defined by the Coinbase Pro API */
 export const GRANULARITY = [60, 300, 900, 3600, 21600, 86400][1];
+/** @brief Price refresh rate (in ms) */
+export const REFRESH_RATE = 200;
